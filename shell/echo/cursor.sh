d9355d4be8aed5b6037639270d4fe802200782eb
jinzx10 <jzx016@hotmail.com>
2019-08-15 19:21:46 -0400

    shell

---
+++ shell/echo/cursor.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+#echo -e "\033[0;33mwarning: parameter not found\033[0m\033[2K\rtqtqtq"
+echo -e "warning: parameter not found"
+echo -e "warning: parameter not found\e[2Ktqtqtq"
+echo -e "warning: parameter not found\033[2K\rtqtqtq"
+echo -e "warning: parameter not found\rtqtqtq"
+echo -e "warning: parameter not found\b\b\b\b\b\b\b\b\b\e[1K"

8974b4fd15c9bba2ad6cace13c8fc9409bec03f4
jinzx10 <jzx016@hotmail.com>
2019-08-21 18:34:34 -0400

    class and function

--- cpp/template/keepcomplex.cpp
+++ cpp/template/keepcomplex.cpp
@@ -1,19 +1,35 @@
 #include <iostream>
 #include <type_traits>
 #include <complex>
+#include <armadillo>
 
-template <bool is_cplx = true>
-struct KeepCplx { static std::complex<double> value(std::complex<double> const& z) {return z;} };
+template <bool is_cplx> using num_t = typename std::conditional<is_cplx, std::complex<double>, double>::type;
 
-template <>
-struct KeepCplx<false> { static double value(std::complex<double> const& z) {return z.real();} };
+// function template
+template <bool is_cplx> num_t<is_cplx> keep_cplx(std::complex<double> const& z) { return z; }
+template <> num_t<false> keep_cplx<false>(std::complex<double> const& z) { return z.real(); }
+
+template <bool is_cplx, typename T> struct KeepCplx { static auto value(T const& z) {return z;} };
+template <typename T> struct KeepCplx<false,T> { static auto value(T const& z) {return real(z);} }; // ADL, std:: or arma::
+
 
 int main() {
 
 	std::complex<double> z(1.1, 2.2);
+	arma::Col<std::complex<double>> zv = {z,3.0*z};
+	arma::cx_vec2 fzv = zv;
 
-	std::cout << KeepCplx<false>::value(z) << std::endl;
-	std::cout << KeepCplx<true>::value(z) << std::endl;
+	std::cout << keep_cplx<false>(z) << std::endl;
+	std::cout << keep_cplx<true>(z) << std::endl;
+
+	std::cout << KeepCplx<false, num_t<true>>::value(z) << std::endl;
+	std::cout << KeepCplx<true, num_t<true>>::value(z) << std::endl;
+	std::cout << KeepCplx<false, arma::cx_vec>::value(zv) << std::endl;
+	std::cout << KeepCplx<true, arma::cx_vec>::value(zv) << std::endl;
+	std::cout << KeepCplx<false, arma::cx_vec2>::value(fzv) << std::endl;
+	std::cout << KeepCplx<true, arma::cx_vec2>::value(fzv) << std::endl;
+
+
 
 	return 0;
 }

b772f7fb0ba95237392a2c616eace988eb8657eb
Zuxin <zuxin@subotnik01.chem.upenn.edu>
2019-08-22 16:42:21 -0400

    hw

--- matlab/utils/randq.m
+++ matlab/utils/randq.m
@@ -8,4 +8,10 @@ q = randn(dim);
 idx_flip = find(diag(r) < 0);
 q(:, idx_flip) = -q(:, idx_flip);
 
+% presumably qr in matlab is computed by Householder reflection,
+% which does not guarantee that all diagonal elements of r are positive,
+% so a sign adjustment is needed.
+% Gram-Schmidt orthogonalization would guarantee that sign convention,
+% but it is numerically unstable.
+
 end

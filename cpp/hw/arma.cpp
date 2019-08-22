a21a51aa439204232e12d78c2fad5ef0fc7b61ac
jinzx10 <jzx016@hotmail.com>
2019-08-22 16:10:54 -0400

    hw

--- cpp/hw/arma.cpp
+++ cpp/hw/arma.cpp
@@ -1,15 +1,20 @@
 #include <armadillo>
 
-double isum(arma::vec2 const& v) {
-	return v(0)+v(1);
-}
+using namespace arma;
 
 int main() {
-	arma::vec2 v2 = {1,2};
+	size_t nx = 10;
+	vec x = linspace<vec>(0, 1, nx);
+	cx_vec z = linspace<cx_vec>(0, 1, nx);
 
-	arma::vec2 u2 = v2 + 1;
+	cx_mat dc = zeros<cx_mat>(nx, 2);
+	dc.col(0) = arma::conv_to<cx_vec>::from(x);
 
-	u2.print();
+	dc.print();
+
+	std::cout << typeid(x).name() << std::endl;
+	std::cout << typeid(z).name() << std::endl;
+	std::cout << typeid(dc).name() << std::endl;
 
 	return 0;
 }

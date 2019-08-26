07171c3a8524bed9cfea04c11c91ce9933ba0e73
jinzx10 <jzx016@hotmail.com>
2019-08-26 00:34:09 -0400

    test

---
+++ cpp/hw/arr.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+
+int main() {
+	size_t const sz_col = 3;
+	size_t const sz_row = 5;
+	int (*A)[sz_col] = nullptr;
+
+	A = new int[sz_row][sz_col];
+
+	for (size_t i = 0; i != sz_row; ++i)
+		for (size_t j = 0; j != sz_col; ++j)
+			A[i][j] = i*10+j;
+
+
+	for (size_t i = 0; i != sz_row; ++i) {
+		for (size_t j = 0; j != sz_col; ++j) {
+			std::cout << A[i][j] << " ";
+		}
+		std::cout << std::endl;
+	}
+
+	return 0;
+}

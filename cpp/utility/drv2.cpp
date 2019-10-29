7c80aea05362e2ef251c1a7e460f94661491cadc
jinzx10 <jzx016@hotmail.com>
2019-10-29 01:33:21 -0400

    rm

--- cpp/utility/drv2.cpp
+++
@@ -1,8 +0,0 @@
-#include "grad.h"
-#include "drv2.h"
-
-std::function<double(double)> drv2(std::function<double(double)> f ) {
-	return grad(grad(f));
-}
-
-
--- cpp/utility/drv2.h
+++
@@ -1,8 +0,0 @@
-#ifndef __DRV2_H__
-#define __DRV2_H__
-
-#include <functional>
-
-std::function<double(double)> drv2(std::function<double(double)> );
-
-#endif

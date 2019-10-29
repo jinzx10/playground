e63a2c520c053323b125c14d7b41e2bf0080e22a
jinzx10 <jzx016@hotmail.com>
2019-10-29 01:20:14 -0400

    grad updated

--- cpp/utility/main.cpp
+++ cpp/utility/main.cpp
@@ -7,16 +7,17 @@ using namespace arma;
 int main() {
 	arma::vec v = {1.5, 3.7};
 
-	auto func = [](arma::vec const& v) {return v(0)*v(0)+v(1)*v(1)*2; };
+	std::function<double(arma::vec)> f = [] (arma::vec const& v) {return v(0)*v(0)+v(1)*v(1)*2; };
+	std::function<double(std::vector<double>)> g = [] (std::vector<double> const& v) {return v[0]*v[0]*0.5+v[1]*v[1]*4.0; };
 
+	auto grad_f = grad(f);
+	grad_f(v).print();
 
-	auto grad_f = grad(func);
+	auto grad_g = grad(g);
+	auto val = grad_g(std::vector<double>{1.1,2.2});
 
-	grad_f({2.0, 0.5}).print();
-
-	uword i = 0;
-	auto fi = [func, i] (arma::vec const& v) {return func();};
-
+	for (auto& e : val)
+		std::cout << e << std::endl;
 
 	return 0;
 }

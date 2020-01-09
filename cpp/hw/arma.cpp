f37601035300015acd934938ead312b6edce7de3
jinzx10 <jzx016@hotmail.com>
2020-01-09 11:06:57 -0500

    hw

--- cpp/hw/arma.cpp
+++ cpp/hw/arma.cpp
@@ -15,23 +15,14 @@ typename std::enable_if<!arma::is_arma_type<T>::value, T>::type zero() {
 
 
 int main() {
-	int sz = 10;
-	int num = 100;
-	
-	mat a = zeros(sz,sz);
-	mat eigvec;
-	vec eigval;
-	int count = 0;
-	for (int i = 0; i != num; ++i) {
-		a = randu(sz,sz);
-		a = a + a.t();
-		eig_sym(eigval, eigvec, a);
-		a = a + (eigval(1)-eigval(0)+1) * (eigvec.col(0) * eigvec.col(0).t());
-		eig_sym(eigval, eigvec, a);
-		count += eigval.is_sorted();
-	}
 
-	std::cout << count << std::endl;
+	arma::mat a = arma::randu(5,5);
+
+	a.print();
+	std::cout << std::endl;
+
+	a(span(3,2), span(3,2)).print();
+	
 
 	return 0;
 }

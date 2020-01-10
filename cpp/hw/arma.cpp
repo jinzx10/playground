f7a8e2932ebd226cf0fc0db9515d76709f169494
jinzx10 <jzx016@hotmail.com>
2020-01-09 23:24:52 -0500

    hw

--- cpp/hw/arma.cpp
+++ cpp/hw/arma.cpp
@@ -3,35 +3,24 @@
 
 using namespace arma;
 
-template <typename T>
-typename std::enable_if<arma::is_arma_type<T>::value, T>::type zero() {
-	return T(arma::fill::zeros);
-}
-
-template <typename T>
-typename std::enable_if<!arma::is_arma_type<T>::value, T>::type zero() {
-	return 0.0;
-}
-
-
 int main() {
-	int sz = 10;
-	int num = 100;
-	
-	mat a = zeros(sz,sz);
-	mat eigvec;
-	vec eigval;
-	int count = 0;
-	for (int i = 0; i != num; ++i) {
-		a = randu(sz,sz);
-		a = a + a.t();
-		eig_sym(eigval, eigvec, a);
-		a = a + (eigval(1)-eigval(0)+1) * (eigvec.col(0) * eigvec.col(0).t());
-		eig_sym(eigval, eigvec, a);
-		count += eigval.is_sorted();
-	}
+	arma::mat a = randu(3,3);
+	arma::vec b = {1,2};
 
-	std::cout << count << std::endl;
+	arma::vec c = arma::conv_to<arma::vec>::from(b>1.5);
+
+	c.print();
+
+	std::cout << typeid(c).name() << std::endl;
+
+
+	std::cout << 1.0 / (std::exp(10000000) + 1.0) << std::endl;
+	std::cout << 1.0 / (std::exp(-10000000) + 1.0) << std::endl;
+	std::cout << 1.0 / (std::exp(1 / arma::datum::eps) + 1.0) << std::endl;
+	std::cout << 1.0 / (std::exp(1 / 1e-18) + 1.0) << std::endl;
+	std::cout << arma::datum::eps << std::endl;
+	
+
 
 	return 0;
 }

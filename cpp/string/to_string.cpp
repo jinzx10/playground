d505fee2cdbee96e0ce424d707dc9a8cd5578f19
jinzx10 <jzx016@hotmail.com>
2020-02-04 00:38:21 -0500

    hw

---
+++ cpp/string/to_string.cpp
@@ -0,0 +1,10 @@
+#include <string>
+#include <iostream>
+
+int main() {
+	unsigned long long sz = 1000;
+	double g = 0.0016;
+	std::cout << std::to_string(sz) << std::endl;
+	std::cout << std::to_string(g) << std::endl;
+	return 0;
+}

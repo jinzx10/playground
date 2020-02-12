d6e0bf4b43f5ba0abf14af7150c2bea203e38068
jinzx10 <jzx016@hotmail.com>
2020-02-12 03:24:49 -0500

    hw

--- cpp/template/is_callable.cpp
+++ cpp/template/is_callable.cpp
@@ -14,6 +14,39 @@ struct has_specific_signature : std::false_type {};
 template <typename F, typename T, typename R>
 struct has_specific_signature<F, T, R, std::void_t< std::is_same<R, decltype( std::declval<F>()(std::declval<T>()))> > > : std::true_type {};
 
+template <typename F, typename ...Args>
+using return_t = decltype( std::declval<F>()( std::declval<Args>()... ) );
+
+template <typename F, typename ...Args>
+struct can_call
+{
+	typedef char yes;
+	typedef char no[2];
+
+	template <typename F1, typename ...Args1>
+	static yes& test(return_t<F1, Args1...>* );
+
+	template <typename F1, typename ...Args1>
+	static no& test(...);
+
+	static const bool value = sizeof(test<F, Args...>(nullptr)) == sizeof(yes);
+
+};
+
+template <typename F, typename R, typename ...Args>
+struct has_signature
+{
+	typedef char yes;
+	typedef char no[2];
+
+	template <typename F1, typename R1, typename ...Args1>
+	static yes& test( typename std::enable_if< std::is_same< R1, return_t<F1, Args1...> >::value, void >::type* );
+
+	template <typename F1, typename R1, typename ...Args1>
+	static no& test(...);
+
+	static const bool value = sizeof(test<F, R, Args...>(nullptr)) == sizeof(yes);
+};
 
 struct foo 
 {
@@ -48,6 +81,16 @@ int main() {
 	//std::cout << is_callable<multiply, float, double>::value << std::endl;
 	std::cout << has_specific_signature<foo, double, int>::value << std::endl;
 	std::cout << has_specific_signature<bar, double, int>::value << std::endl;
+
+	std::cout << can_call<foo>::value << std::endl;
+	std::cout << can_call<foo, int>::value << std::endl;
+	std::cout << can_call<bar, int>::value << std::endl;
+
+	std::cout << has_signature<foo,int>::value << std::endl;
+	std::cout << has_signature<foo,int,int>::value << std::endl;
+	std::cout << has_signature<bar, int, int>::value << std::endl;
+	std::cout << has_signature<tee, float>::value << std::endl;
+	std::cout << has_signature<tee, int, double,int>::value << std::endl;
 
 	return 0;
 }
--- cpp/template/return_type.cpp
+++ cpp/template/return_type.cpp
@@ -3,13 +3,35 @@
 template <typename F, typename ...Args>
 using return_t = decltype( std::declval<F>()( std::declval<Args>()... ) );
 
+template <typename F, typename ...Args>
+static bool test(return_t<F, Args...>* ) {
+	return true;
+}
+
+template <typename ...C>
+static bool test(...) {
+	return false;
+}
+
+
 struct foo
 {
+	void operator()(int) {};
+};
+
+struct bar
+{
+	int operator()(int) {return 1;};
+};
 
 
 int main() {
 
+	std::cout << typeid( return_t<foo, int> ).name() << std::endl;
+	std::cout << typeid( return_t<bar, int> ).name() << std::endl;
 
+	std::cout << test<foo,int>(nullptr) << std::endl;
+	std::cout << test<foo,int,int>(nullptr) << std::endl;
 
 	return 0;
 }

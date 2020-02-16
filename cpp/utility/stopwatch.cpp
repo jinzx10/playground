2e0b1b412fe6a4ab162c612f1b3c583c94d7463b
jinzx10 <jzx016@hotmail.com>
2020-02-15 23:37:08 -0500

    hw

--- cpp/utility/stopwatch.cpp
+++ cpp/utility/stopwatch.cpp
@@ -12,32 +12,14 @@ int main(int, char**argv) {
 
 	readargs(argv, sz, nt);
 
+	std::cout << "sz = " << sz << std::endl;
+	std::cout << "nt = " << nt << std::endl;
+
 	arma::mat a = arma::ones(sz,sz);
 	auto f = [] (arma::mat const& a) -> arma::mat { return arma::exp(a); };
 	auto matmul = [] (arma::mat const& m, arma::mat const& n) -> arma::mat {return m*n;};
 
-	/*
-	sw.timeit<0>(f, a);
-	sw.timeit<10>(f, a);
-	sw.timeit<100>(f, a);
-
-	sw.timeit<0>(matmul, a, a);
-	sw.timeit<10>(matmul, a, a);
-	sw.timeit<100>(matmul, a, a);
-	*/
-
-
-	/*
-	sw.timeit(0, f, a);
-	sw.timeit(1, f, a);
-	sw.timeit(f, a);
-	sw.timeit(100, f, a);
-
-	sw.timeit(0, matmul, a, a);
-	sw.timeit(1, matmul, a, a);
-	sw.timeit(matmul, a, a);
-	sw.timeit(100, matmul, a, a);
-	*/
+	auto h = [&a] () -> arma::vec { return arma::eig_sym(a); };
 
 	sw.timeit(f, a);
 	sw.timeit(matmul, a, a);
@@ -50,6 +32,8 @@ int main(int, char**argv) {
 
 	sw.timeit("eig", nt, f, a);
 	sw.timeit("matmul", nt, matmul, a, a);
+
+	sw.timeit("test eig", h);
 
 	sw.run();
 	arma::mat b = arma::exp(a);

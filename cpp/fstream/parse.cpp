08051fb016aa72f1104ac2aa88be171bb4e3d5ab
jinzx10 <jzx016@hotmail.com>
2020-02-21 01:31:38 -0500

    hw

--- cpp/fstream/parse.cpp
+++ cpp/fstream/parse.cpp
@@ -1,68 +1,25 @@
-#include <fstream>
-#include <iostream>
-#include <sstream>
-#include <vector>
-
-struct P
-{
-	P(std::vector<std::string> const& keys_) : keys(keys_), n_keys(keys.size()), vals(n_keys) {}
-	std::vector<std::string> keys;
-	size_t n_keys;
-	std::vector<std::string> vals;
-	void parse(std::string const& file) {
-		std::fstream fs(file);
-		std::string str;
-		while (std::getline(fs, str)) {
-			for (size_t i = 0; i != keys.size(); ++i) {
-				auto pos = str.find(keys[i]);
-				if (pos != std::string::npos) {
-					str.erase(pos, keys[i].length());
-					vals[i] = str;
-					break;
-				}
-			}
-		}
-	}
-
-	template <int N = 0, typename T>
-	void pour(T& val) {
-		if (N >= n_keys) {
-			std::cerr << "Too many variables." << std::endl;
-			return;
-		}
-		std::stringstream ss;
-		ss << vals[N];
-		ss >> val;
-	}
-
-	template <int N = 0, typename T, typename ...Ts>
-	void pour(T& val, Ts& ...args) {
-		if (N >= n_keys) {
-			std::cerr << "Too many variables." << std::endl;
-			return;
-		}
-		std::stringstream ss;
-		ss << vals[N];
-		ss >> val;
-		pour<N+1, Ts...>(args...);
-	}
-};
+#include "../utility/widgets.h"
 
 
 int main() {
-	P p({"size", "value", "dir"});
+	Parser p({"size", "value", "dir", "age", "name", "job"});
 	p.parse("input.txt");
 
 	int sz;
 	double val;
 	std::string dir;
-	double tmp;
+	unsigned int age;
+	std::string name;
+	bool job;
 
-	p.pour(sz, val, dir, tmp);
+	p.pour(sz, val, dir, age, name, job);
 
 	std::cout << "size = " << sz << std::endl;
 	std::cout << "value = " << val << std::endl;
 	std::cout << "dir = " << dir << std::endl;
+	std::cout << "age = " << age << std::endl;
+	std::cout << "name = " << name << std::endl;
+	std::cout << std::boolalpha << "job = " << job << std::endl;
 
 	return 0;
 }

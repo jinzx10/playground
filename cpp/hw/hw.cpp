e444b07634e42399faa8cd0635afa0571b3b9458
jinzx10 <jzx016@hotmail.com>
2020-04-24 20:13:38 -0400

    hw

--- cpp/hw/hw.cpp
+++ cpp/hw/hw.cpp
@@ -18,30 +18,16 @@ typename std::enable_if< R::is_row && C::is_col, arma::Mat<decltype(std::declval
 */
 int main(int, char**argv) {
 
-	uword sz, nt;
+	std::cout << "ready" << std::endl;
+	std::cout << "ready2" << std::endl;
+	for (int i = 0; i != 10; ++i) {
+		
+		printf("\033[A\33[2K\r");
+		std::cout << "i = " << i << std::endl;
+		std::system("sleep 0.5");
+	}
 
-	readargs(argv, sz, nt);
-
-	vec a = linspace<vec>(1, sz, sz);
-	rowvec b = linspace<rowvec>(0, sz-1, sz);
-
-	Stopwatch sw;
-
-	auto f = [](vec const& v, rowvec const& r) { return bcast_plus(v,r); };
-
-	sw.timeit(nt, f, a, b);
-
-	mat c = std::plus<>()(repmat(a, 1, b.n_elem).eval().each_row(), b);
-
-	a.print();
 	std::cout << std::endl;
-	b.print();
-	std::cout << std::endl;
-	c.print();
-	
-	pow(a, 3).print();
-
-	
 
     return 0;
 }

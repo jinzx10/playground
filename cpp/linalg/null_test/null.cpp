b3fb564d1af78684ea367b8d7c49e4e59ea0674a
jinzx10 <jzx016@hotmail.com>
2020-05-10 03:29:22 -0400

    hw

--- cpp/linalg/null_test/null.cpp
+++ cpp/linalg/null_test/null.cpp
@@ -8,16 +8,18 @@ int main() {
 	mat Zt;
 	Zt.load("Zt.dat");
 
+	mat I = eye(size(Zt));
+	cout << "size: " << size(Zt) << std::endl;
+	std::cout << "dev from I = " << norm(I-Zt) << std::endl;
 	mat ns;
 	bool null_status = null(ns, Zt);
 	cout << boolalpha << "null: " << null_status << endl;
 
 
-	/*
-	vec sv;
-	bool svd_status = svd(sv, Zt);
+	mat U, V;
+	vec S;
+	bool svd_status = svd(U, S, V, Zt);
 	cout << boolalpha << "svd: " << svd_status << endl;
-	*/
 
 
 	return 0;

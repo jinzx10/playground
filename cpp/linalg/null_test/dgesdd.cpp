d02f5e9817ac9eb4a8d8509fcdb3c182090a8543
jinzx10 <jzx016@hotmail.com>
2020-05-10 01:01:52 -0400

    hw

--- cpp/linalg/null_test/dgesdd.cpp
+++ cpp/linalg/null_test/dgesdd.cpp
@@ -6,7 +6,7 @@ using namespace arma;
 
 int main() {
 
-	mat A, S, U, V, A_pre, A_raw, A_fin;
+	mat A, S, U, VT, A_pre, A_raw, A_fin, Ss;
 	A_fin.load("A_fin.txt");
 	A_pre.load("A_pre.txt");
 	A_raw.load("A_raw.txt");
@@ -21,6 +21,7 @@ int main() {
 
 	std::cout << "A_raw and A_pre diff = " << norm(A_raw-A_pre) << std::endl;
 	std::cout << "A_pre and A_fin diff = " << norm(A_fin-A_pre) << std::endl;
+	std::cout << std::endl << std::endl;
 
 	///////////////////////////////////////////////////////////////////////
 	//							C interface
@@ -30,7 +31,7 @@ int main() {
 	int n = A.n_cols;
 
 	U.set_size(m,m);
-	V.set_size(n,n);
+	VT.set_size(n,n);
 	S.set_size(std::min(m,n));
 
 	int lda = m;
@@ -39,10 +40,14 @@ int main() {
 
 	int info;
 
-	info = LAPACKE_dgesdd( LAPACK_COL_MAJOR, jobz, m, n, A.memptr(), lda, S.memptr(), U.memptr(), ldu, V.memptr(), ldvt );
+	info = LAPACKE_dgesdd( LAPACK_COL_MAJOR, jobz, m, n, A.memptr(), lda, S.memptr(), U.memptr(), ldu, VT.memptr(), ldvt );
 
+	Ss.zeros(m,n);
+	Ss.diag() = S;
 	std::cout << "C interface: info = " << info << std::endl;
 	std::cout << "C dgesdd diff = " << norm(A-A_raw) << std::endl;
+	std::cout << "A-U*S*VT = " << norm(A_raw-U*Ss*VT) << std::endl;
+	std::cout << std::endl << std::endl;
 
 	///////////////////////////////////////////////////////////////////////
 	//						Fortran interface
@@ -54,16 +59,21 @@ int main() {
 	int* iwork = new int[8*n];
         
 	A = A_raw;
-	dgesdd(&jobz, &m, &n, A.memptr(), &lda, S.memptr(), U.memptr(), &ldu, V.memptr(), &ldvt, 
+	dgesdd(&jobz, &m, &n, A.memptr(), &lda, S.memptr(), U.memptr(), &ldu, VT.memptr(), &ldvt, 
 			&wkopt, &lwork, iwork, &info);
 
 	lwork = (int)wkopt;
 	work = new double[lwork];
 
-	dgesdd(&jobz, &m, &n, A.memptr(), &lda, S.memptr(), U.memptr(), &ldu, V.memptr(), &ldvt, 
+	dgesdd(&jobz, &m, &n, A.memptr(), &lda, S.memptr(), U.memptr(), &ldu, VT.memptr(), &ldvt, 
 			work, &lwork, iwork, &info);
 
+	Ss.zeros(m,n);
+	Ss.diag() = S;
 	std::cout << "Fortran interface: info = " << info << std::endl;
+	std::cout << "Fortran dgesdd diff = " << norm(A-A_raw) << std::endl;
+	std::cout << "A-U*S*VT = " << norm(A_raw-U*Ss*VT) << std::endl;
+	std::cout << std::endl << std::endl;
 
 	return 0;
 }

a3e3c35e2854a9063f9e7a04e344d6945e39d48b
jinzx10 <jzx016@hotmail.com>
2023-12-15 02:45:07 +0800

    upfread

--- python/orbgen/upfread.py
+++ python/orbgen/upfread.py
@@ -1,9 +1,14 @@
 import re
 
 def _next_key(lines):
-    i_angle = lines.find('<')
-    i_space = lines[i_angle:].find(' ')
-    return lines[i_angle+1 : i_angle + i_space] if i_angle >= 0 else ''
+    '''
+    Extracts the string between the first pair of '<' and ' '
+    or '<' and '>'.
+
+    '''
+    result = re.search('<([^!]+?)( |>)', lines)
+    return result.group(1) if result else None
+
 
 def _attrs2dict(attrs):
     '''
@@ -20,48 +25,75 @@ def _attrs2dict(attrs):
         A dictionary of key-value pairs.
 
     '''
-    return { k: v for k, v in re.findall('([^ ]+?)=" *([^= ]*) *"', attrs)}
+    return { k: v.strip(' ') for k, v in re.findall('([^ ]+?)="([^=]*)"', attrs)}
+
 
 def _parse(content):
-    result = {}
+    '''
+    Recursively parse the content.
 
-    # Get the next key
-    key = _next_key(content)
-    idx = 0
-    while (len(key) > 0):
-        print(key)
-        key, stride = _next_key(content[idx:])
-        idx += stride
+    A UPF block has either one of the following formats:
 
-    result = re.search('<{0}(.*?)>(.*)</{0}>'.format(next_key), content)
-    attrs = result.group(1) if result else ''
-    print(attributes)
-    data = result.group(2)
+        <KEY ATTR1="..." ATTR2="..." ...>DATA</KEY>     (a block with data)
+        <KEY ATTR1="..." ATTR2="..." .../>              (a block without data)
 
-    return result
+    Each UPF block will be converted to a dict entry. If DATA contains no
+    UPF block, the entry (key-value pair) will be
+
+        KEY: {ATTR1: ..., ATTR2: ..., 'data': DATA}
+
+    If DATA contains further UPF blocks, the 'data': DATA part will be replaced
+    by the parsed UPF blocks, i.e.,
+    
+        KEY: {ATTR1: ..., ATTR2: ..., NESTED_KEY1: {...}, NESTED_KEY2: {...}, ...}
+
+    '''
+    block = {}
+
+    while key := _next_key(content):
+        if (result := re.search('<{0}([^>]*)>(.*)</{0}>'.format(key), content)) is None:
+            result = re.search('<{0}([^>]*)(.*?)/>'.format(key), content)
+        assert result.group(0).count('<') == result.group(0).count('>'), 'Unmatched < and >'
+
+        attrs, data = result.group(1), result.group(2)
+        block[key] = _attrs2dict(attrs) | (_parse(data) if _next_key(data) else {'data': data})
+        content = content[result.end(0):]
+
+    return block
 
 
 def read_upf(fpath):
     with open(fpath, 'r') as f:
         content = f.read()
+    content = content.replace('\n', ' ') # remove all newlines (for regex)
+    return _parse(content)
 
-    content = content.replace('\n', ' ')
-
-    _parse(content)
-
-    teststr='date="150105" comment="" element="In" pseudo_type="NC" relativistic="scalar" is_ultrasoft="F" '
-    print(result)
-
-    print(_attrs2dict(teststr))
 
 ############################################################
 #                           Test
 ############################################################
 import unittest
-class TestUPFRead(unittest.TestCase):
-    def test_read_upf(self):
-        read_upf('./testfiles/In_ONCV_PBE-1.0.upf')
 
+class TestUPFRead(unittest.TestCase):
+
+    def test_next_key(self):
+        teststr = '<UPF version="2.0.1">'
+        self.assertEqual(_next_key(teststr), 'UPF')
+
+        teststr = '<PP_INFO>'
+        self.assertEqual(_next_key(teststr), 'PP_INFO')
+
+    
+    def test_attrs2dict(self):
+        teststr = 'key1=" value1"  key2="value2 "  key3=" value3 "'
+        self.assertEqual(_attrs2dict(teststr), {'key1': 'value1', 'key2': 'value2', 'key3': 'value3'})
+
+
+    def test_read_upf(self):
+        upf = read_upf('./testfiles/In_ONCV_PBE-1.0.upf')
+        self.assertEqual(upf['UPF']['version'], '2.0.1')
+        self.assertEqual(upf['UPF']['PP_HEADER']['generated'], 'Generated using ONCVPSP code by D. R. Hamann')
 
 if __name__ == '__main__':
     unittest.main()
+

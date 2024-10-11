4150e8496925a5c6bbeee23fc6da7c026360f2dc
jinzx10 <jzx016@hotmail.com>
2024-10-11 20:57:26 +0800

    becke test updated

--- python/grid/becke.py
+++ python/grid/becke.py
@@ -27,9 +27,10 @@ def becke_part(R, r):
     Becke partition weights.
     
     '''
-    #s = s_becke
-    s = s_stratmann
-    P = np.ones(len(R))
+    s = s_becke
+    #s = s_stratmann
+
+    P = np.ones(R.shape[0])
     for I, RI in enumerate(R):
         for J, RJ in enumerate(R):
             if I == J:
@@ -48,38 +49,64 @@ def becke_part(R, r):
 import unittest
 import matplotlib.pyplot as plt
 
-from radial import baker
+from radial import baker, murray
 from delley import delley
 
 class TestBecke(unittest.TestCase):
     pass
 
 
-def func(r, R1, R2, a1, a2):
-    return np.exp(-a1 * np.linalg.norm(r - R1.reshape((1,3)), axis=1)**2) \
-            + np.exp(-a2 * np.linalg.norm(r - R2.reshape((1,3)), axis=1)**2)
+#def func(r, R1, R2, a1, a2):
+#    return np.exp(-a1 * np.linalg.norm(r - R1.reshape((1,3)), axis=1)**2) \
+#            + np.exp(-a2 * np.linalg.norm(r - R2.reshape((1,3)), axis=1)**2)
+
+def func(r, R, a):
+    '''
+    Test function.
+
+        f(r) = \sum_i exp(-a[i] * |r-R[i]|^2)
+
+    Parameters
+    ----------
+    r : np.ndarray, shape=(3)
+        The grid points.
+    R : np.ndarray, shape=(nR,3)
+        The atomic positions.
+    a : np.ndarray, shape=(nR,)
+        The exponents.
+
+    '''
+    val = 0.0
+    for ai, Ri in zip(a, R):
+        val += np.exp(-ai * np.linalg.norm(r - Ri)**2)
+    return val
+
+
 
 if __name__ == '__main__':
     #unittest.main()
 
-    
     #mu = np.linspace(-1, 1, 100)
     #plt.plot(mu, s_becke(mu), label='Becke')
     #plt.plot(mu, s_stratmann(mu), label='Stratmann')
     #plt.legend()
     #plt.show()
+    #exit()
 
-    R1 = np.array([0, 0, 0])
-    R2 = np.array([0, 0, 1])
-    a1 = 1.2
-    a2 = 2.1
+    R = np.array([
+        [0, 0, 0],
+        [0, 0, 3],
+        [0, 3, 0],
+        ])
 
-    #r = np.random.randn(3,10)
-    #f = func(r, R1, R2, a1, a2)
-    #print(f)
+    a = np.array([1.1, 2.2, 3.3])
 
-    r_rad, w_rad = baker(20, 7, 2)
-    r_ang, w_ang = delley(5)
+    nR = R.shape[0]
+    print(f'nR = {nR}')
+
+    r_rad, w_rad = baker(30, 5, 2)
+    #r_rad, w_rad = murray(40, 3.0)
+    r_ang, w_ang = delley(40)
 
     r = []
     w = []
@@ -90,39 +117,20 @@ if __name__ == '__main__':
 
     r = np.array(r)
     w = np.array(w)
-
-    grid1 = r + R1
-    grid2 = r + R2
-
-    #val = 0.0
-    #ref = (np.pi/a1)**1.5
-    #for wi, ri in zip(w, grid1):
-    #    val += wi * np.exp(-a1*np.linalg.norm(ri-R1)**2)
-
-    #val *= 4 * np.pi
-    #print(f'val = {val}')
-    #print(f'ref = {ref}')
-
+    print(f'number of grid points = {r.shape[0]}')
+    print(f'radial max = {r_rad[-1]}')
 
     val = 0.0
-    for wi, ri in zip(w, grid1):
-        w_becke = becke_part([R1, R2], ri)
-        val += wi * func(ri, R1, R2, a1, a2)[0] * w_becke[0]
-
-    for wi, ri in zip(w, grid2):
-        w_becke = becke_part([R1, R2], ri)
-        val += wi * func(ri, R1, R2, a1, a2)[0] * w_becke[1]
+    for iR in range(nR):
+        for wi, ri in zip(w, r):
+            w_becke = becke_part(R, ri + R[iR])
+            val += wi * func(ri + R[iR], R, a) * w_becke[iR]
 
     val *= 4 * np.pi
-    ref = (np.pi/a1)**1.5 + (np.pi/a2)**1.5
+    ref = np.sum((np.pi/a)**1.5)
     print(f'val = {val}')
     print(f'ref = {ref}')
 
-
-
-
-    #print(grid.shape)
-    #print(weight.shape)
 
     #fig = plt.figure()
     #ax = fig.add_subplot(projection='3d')
